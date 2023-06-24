d82e3bb44ddcccc719474539044f1fbedd043086
ysengokucd <ysengoku.tourisme@gmail.com>
2023-06-24 23:12:40 +0200

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by apprenant71 on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
